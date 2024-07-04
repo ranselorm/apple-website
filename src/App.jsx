48742e7cf23d20e12eb0a1fce6b995cea29fd6e6
Selorm <ranselorm@outlook.com>
2024-07-04 22:23:00 +0000

    Finish navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import { useState } from "react";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Highlights from "./components/Highlights";
 
-import "./App.css";
-
-function App() {
+const App = () => {
   return (
-    <>
-      <h1>Hello Apple</h1>
-    </>
+    <main className="">
+      <Navbar />
+      {/* <Hero />
+      <Highlights /> */}
+    </main>
   );
-}
+};
 
 export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,27 @@
+import { appleImg, bagImg, searchImg } from "../utils";
+import { navLists } from "../constants/index";
+const Navbar = () => {
+  return (
+    <header className="w-full py-5 sm:px-10 px-5 flex items-center justify-center">
+      <nav className="flex w-full screen-max-width">
+        <img src={appleImg} alt="Apple" width={14} height={18} />
+        <div className="flex flex-1  justify-center max-sm:hidden">
+          {navLists.map((navItem, index) => (
+            <div
+              key={index}
+              className="cursor-pointer px-5 text-sm text-gray hover:text-white transition-all"
+            >
+              {navItem}
+            </div>
+          ))}
+        </div>
+        <div className="flex items-baseline gap-7 max-sm:flex-1 max-sm:justify-end">
+          <img src={searchImg} alt="search" with={18} height={18} />
+          <img src={bagImg} alt="bag" with={18} height={18} />
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
