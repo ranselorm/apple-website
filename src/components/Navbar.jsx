ed57a677687a427c6341f996409793a2d24541aa
Selorm <ranselorm@outlook.com>
2024-07-05 22:28:53 +0000

    Fix navbar padding

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { navLists } from "../constants/index";
 
 const Navbar = () => {
   return (
-    <header className="w-full py-5 sm:px-10 px-5 flex items-center justify-center bg-gray">
+    <header className="w-full py-5 sm:px-10 px-5 flex items-center justify-center">
       <nav className="flex w-full screen-max-width">
         <img src={appleImg} alt="Apple" width={14} height={18} />
         <div className="flex flex-1  justify-center max-sm:hidden">
