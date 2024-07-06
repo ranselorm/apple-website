4ca504a36d744914a2fd49a616e90ee428b88774
Selorm <ranselorm@outlook.com>
2024-07-06 07:19:08 +0000

    Add hero video

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,29 @@
+import { useEffect, useState } from "react";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
-import React from "react";
+
+import { heroVideo, smallHeroVideo } from "../utils";
 
 const Hero = () => {
+  const [videoSrc, setVideoSrc] = useState(
+    window.innerWidth < 760 ? smallHeroVideo : heroVideo
+  );
+
+  const handleVideoSrcSet = () => {
+    if (window.innerWidth < 760) {
+      setVideoSrc(smallHeroVideo);
+    } else {
+      setVideoSrc(heroVideo);
+    }
+  };
+  useEffect(() => {
+    window.addEventListener("resize", handleVideoSrcSet);
+
+    return () => {
+      window.removeEventListener("resize", handleVideoSrcSet);
+    };
+  }, []);
+
   useGSAP(() => {
     gsap.to("#hero-title", { opacity: 1, delay: 0.5 });
   }, []);
@@ -12,6 +33,17 @@ const Hero = () => {
         <p id="hero-title" className="hero-title">
           iPhone 15 Pro
         </p>
+        <div className="md:w-10/12 w-9/10">
+          <video
+            autoPlay
+            playsInline={true}
+            muted
+            key={videoSrc}
+            className="pointer-events-none"
+          >
+            <source src={videoSrc} type="video/mp4" />
+          </video>
+        </div>
       </div>
     </section>
   );
